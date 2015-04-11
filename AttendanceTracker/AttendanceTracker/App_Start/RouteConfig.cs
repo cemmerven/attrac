87ba73729b4c6c02507c05aadc3b94c4ea5aba6e
cemmerven <cemmerven@gmail.com>
2015-04-11 04:53:44 +0300

    added static content samples and "static" directory

--- AttendanceTracker/AttendanceTracker/App_Start/RouteConfig.cs
+++ AttendanceTracker/AttendanceTracker/App_Start/RouteConfig.cs
@@ -10,6 +10,8 @@ namespace AttendanceTracker {
       public static void RegisterRoutes( RouteCollection routes ) {
          routes.IgnoreRoute( "{resource}.axd/{*pathInfo}" );
 
+         //routes.IgnoreRoute("static/{*pathInfo}");
+
          routes.MapRoute(
              name: "Default",
              url: "{controller}/{action}/{id}",
