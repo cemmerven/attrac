bc415806a054189885966783ef0ad417520202a9
cemmerven <cemmerven@gmail.com>
2015-04-17 14:43:17 +0300

    added HtmlForm.cshtml

--- AttendanceTracker/AttendanceTracker/Controllers/TestController.cs
+++ AttendanceTracker/AttendanceTracker/Controllers/TestController.cs
@@ -8,13 +8,30 @@ namespace AttendanceTracker.Controllers
 {
     public class TestController : Controller
     {
+
+      public ActionResult Hello() { // FormCollection (Request.From) vs Request.QueryString
+         return View( "Hello" );
+      }
  
-      public ActionResult HelloTo() { // FormCollection (Request.Fom) vs Request.QueryString
+      public ActionResult HelloTo() { // FormCollection (Request.From) vs Request.QueryString
          return View( "HelloTo" );
       }
 
       public ActionResult FormData( string firstname, int number = int.MinValue ) { 
          return View( "HelloWithParams" );
+      }
+
+      public ActionResult HtmlForm() { 
+         return View( "HtmlForm" );
+      }
+      public ActionResult HtmlFormData() { 
+
+         //Request.Form
+         if ( String.IsNullOrWhiteSpace( Request.Form["metin"] ) )
+           return View( "OKNOT" );
+         else
+           return View( "OK" );
+      
       }
 
     }
