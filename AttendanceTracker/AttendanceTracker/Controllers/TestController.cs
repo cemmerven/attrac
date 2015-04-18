3c09f83bf0cf67d0689c9c4c28d52f4ff3f7de24
cemmerven <cemmerven@gmail.com>
2015-04-18 03:07:27 +0300

    added html form samples

--- AttendanceTracker/AttendanceTracker/Controllers/TestController.cs
+++ AttendanceTracker/AttendanceTracker/Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AttendanceTracker.Models;
 
 namespace AttendanceTracker.Controllers
 {
@@ -21,18 +22,45 @@ namespace AttendanceTracker.Controllers
          return View( "HelloWithParams" );
       }
 
-      public ActionResult HtmlForm() { 
-         return View( "HtmlForm" );
+      //-----------------------------------------------------------------------
+
+      public ActionResult HtmlForm1() { 
+         return View( "HtmlForm1" );
       }
-      public ActionResult HtmlFormData() { 
+      public ActionResult HtmlFormData1() { 
 
          //Request.Form
-         if ( String.IsNullOrWhiteSpace( Request.Form["metin"] ) )
+         if ( String.IsNullOrWhiteSpace( Request.Form[ "metin" ] ) )
            return View( "OKNOT" );
          else
            return View( "OK" );
+      }
+
+      //-----------------------------------------------------------------------
+      
+       public ActionResult HtmlForm2() { 
+         return View( "HtmlForm2" );
+      }
+
+      public ActionResult HtmlFormData2(/* TODO : parametre listesini HtmlForm2.cshtm deki forma göre oluştur */) { 
+
+         return new EmptyResult();
       
       }
 
-    }
-}
+      //-----------------------------------------------------------------------
+
+      public ActionResult HtmlForm3() { 
+         return View( "HtmlForm3" );
+      }
+      public ActionResult HtmlFormData3( /* TODO : Complex binding person */ ) { 
+
+         return new EmptyResult();
+      
+      }
+
+      //-----------------------------------------------------------------------
+
+    }//class
+
+}//namespace
